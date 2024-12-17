836c8c5b989bfff6ebe809a29cedf18f474bb213
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-18 05:28:50 +0800

    Update Port class to remove faction system

--- Port.cs
+++ Port.cs
@@ -1,59 +1,19 @@
 using UnityEngine;
 using CSM.Base;
 
-public class Port : SeaEntityBase
+public class Port : MonoBehaviour
 {
-    [SerializeField]
-    private FactionType owningFaction = FactionType.None;
-    
     [SerializeField]
     private string portName;
     
-    public FactionType OwningFaction => owningFaction;
     public string PortName => portName;
-
-    public override void SetFaction(FactionType newFaction)
+    
+    private void Start()
     {
-        if (owningFaction != newFaction)
+        if (string.IsNullOrEmpty(portName))
         {
-            var oldFaction = owningFaction;
-            owningFaction = newFaction;
-            
-            // Notify FactionManager of the change
-            if (FactionManager.Instance != null)
-            {
-                FactionManager.Instance.HandlePortCapture(newFaction, this);
-            }
-            
-            Debug.Log($"Port {portName} changed ownership from {oldFaction} to {newFaction}");
-        }
-    }
-
-    protected override void Start()
-    {
-        base.Start();
-        
-        // Register with initial faction
-        if (owningFaction != FactionType.None && FactionManager.Instance != null)
-        {
-            var factionData = FactionManager.Instance.GetFactionData(owningFaction);
-            if (factionData != null)
-            {
-                factionData.AddPort(this);
-            }
-        }
-    }
-
-    protected override void OnDestroy()
-    {
-        // Unregister from current faction
-        if (owningFaction != FactionType.None && FactionManager.Instance != null)
-        {
-            var factionData = FactionManager.Instance.GetFactionData(owningFaction);
-            if (factionData != null)
-            {
-                factionData.RemovePort(this);
-            }
+            portName = $"Port_{gameObject.name}";
+            Debug.LogWarning($"Port name was empty, assigned default name: {portName}");
         }
     }
 }
