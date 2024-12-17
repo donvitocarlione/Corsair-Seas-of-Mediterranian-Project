c9130465c1a7d7421a044d7e1671c6427c99ec33
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-18 06:23:01 +0800

    Add missing properties and methods to Ship class to fix CS1061 errors

--- Ship.cs
+++ Ship.cs
@@ -4,6 +4,27 @@ public class Ship : MonoBehaviour
 {
     [SerializeField] protected string shipName;
     public string Name => shipName;
+    public string ShipName => shipName; // Additional property for backward compatibility
+
+    [SerializeField] protected float attackRange = 20f;
+    public float AttackRange => attackRange;
+
+    [SerializeField] protected float firingArc = 60f;
+    public float FiringArc => firingArc;
+
+    [SerializeField] protected float attackDamage = 10f;
+    public float AttackDamage => attackDamage;
+
+    [SerializeField] protected int maxAmmo = 10;
+    [SerializeField] protected float reloadTime = 3f;
+    protected float reloadTimer;
+    protected int currentAmmo;
+    public int CurrentAmmo => currentAmmo;
+
+    [SerializeField] protected Transform[] firingPoints;
+    
+    public bool IsSinking { get; protected set; } = false;
+    public bool IsSelected { get; protected set; } = false;
 
     protected ShipMovement movement;
     protected FiringSystem firingSystem;
@@ -18,6 +39,7 @@ public class Ship : MonoBehaviour
         movement = GetComponent<ShipMovement>();
         firingSystem = GetComponent<FiringSystem>();
         shipOwner = GetComponent<Pirate>();
+        currentAmmo = maxAmmo;
     }
 
     protected virtual void Start()
@@ -28,5 +50,49 @@ public class Ship : MonoBehaviour
         }
     }
 
-    // Add other ship-specific functionality here
+    protected virtual void Update()
+    {
+        if (currentAmmo < maxAmmo)
+        {
+            reloadTimer -= Time.deltaTime;
+            if (reloadTimer <= 0)
+            {
+                currentAmmo++;
+                reloadTimer = reloadTime;
+            }
+        }
+    }
+
+    public virtual void TakeDamage(float amount)
+    {
+        // Implement damage logic in derived classes
+        Debug.Log($"[Ship] {shipName} took {amount} damage!");
+    }
+
+    public virtual Transform[] GetFiringPoints()
+    {
+        return firingPoints;
+    }
+
+    public bool CanFire => CurrentAmmo > 0;
+
+    public virtual void SetSelected(bool selected)
+    {
+        IsSelected = selected;
+    }
+
+    public virtual void StartSinking()
+    {
+        IsSinking = true;
+        // Derived classes should implement specific sinking behavior
+    }
+
+    public virtual void ConsumeAmmo()
+    {
+        if (currentAmmo > 0)
+        {
+            currentAmmo--;
+            reloadTimer = reloadTime;
+        }
+    }
 }
