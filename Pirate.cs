1b845e45b36ae9d364a41c5709fae734548fd28f
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-18 05:27:07 +0800

    Update Pirate class to remove faction system

--- Pirate.cs
+++ Pirate.cs
@@ -2,26 +2,9 @@ using UnityEngine;
 using System.Collections.Generic;
 using CSM.Base;
 
-
 public class Pirate : MonoBehaviour, IShipOwner
 {
     protected List<Ship> ownedShips = new List<Ship>();
-    [SerializeField]
-    private FactionType faction;
-
-    public FactionType Faction
-    {
-        get => faction;
-        protected set => faction = value;
-    }
-
-    public virtual void SetFaction(FactionType newFaction)
-    {
-        if (!object.Equals(newFaction, Faction))
-        {
-            this.Faction = newFaction;
-        }
-    }
 
     public virtual void AddShip(Ship ship)
     {
@@ -35,14 +18,12 @@ public class Pirate : MonoBehaviour, IShipOwner
         {
             ownedShips.Add(ship);
             ship.SetOwner(this);
-            ship.Initialize(Faction, ship.ShipName);
+            ship.Initialize(ship.ShipName);
 
-             if (!(this is Player) && ship.GetComponent<AIShipController>() == null)
+            if (!(this is Player) && ship.GetComponent<AIShipController>() == null)
             {
                 ship.gameObject.AddComponent<AIShipController>().Initialize(ship);
-                Debug.Log($"Added AI controller to {ship.ShipName} in pirate {GetType().Name} fleet");
             }
-            Debug.Log($"Added ship {ship.ShipName} to {GetType().Name}'s fleet");
         }
     }
 
@@ -64,15 +45,13 @@ public class Pirate : MonoBehaviour, IShipOwner
             if (!(this is Player) && ship.TryGetComponent<AIShipController>(out var aiController))
             {
                 Destroy(aiController);
-                Debug.Log($"Removed AI controller from ship {ship.ShipName} in {GetType().Name} fleet");
             }
-            Debug.Log($"Removed ship {ship.ShipName} from {GetType().Name}'s fleet");
         }
     }
 
     public virtual void SelectShip(Ship ship)
     {
-         if (ship == null)
+        if (ship == null)
         {
             Debug.LogError("Attempting to select a null ship!");
             return;
@@ -87,7 +66,7 @@ public class Pirate : MonoBehaviour, IShipOwner
                     ownedShip.Deselect();
                 }
             }
-             ship.Select();
+            ship.Select();
         }
     }
 
