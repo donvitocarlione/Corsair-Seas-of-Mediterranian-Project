06292c50f2b761f37c41d0770db0fd5309f5d1ce
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-19 07:46:45 +0800

    Update Faction.cs to ScriptableObject and remove old system references: - Convert from MonoBehaviour to ScriptableObject - Remove FactionType references - Add proper faction properties - Improve relationship checking - Add influence handling - Add editor validation

--- Faction.cs
+++ Faction.cs
@@ -1,46 +1,52 @@
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Faction : MonoBehaviour
+[CreateAssetMenu(fileName = "New Faction", menuName = "Game/Faction")]
+public class Faction : ScriptableObject
 {
-    public FactionType factionType;
-    public string factionName;
-    public List<Pirate> members = new List<Pirate>();
-    public List<Port> controlledPorts = new List<Port>();
+    [SerializeField] private string factionName;
+    [SerializeField] private Sprite factionFlag;
+    [SerializeField] private Color factionColor = Color.white;
+    [SerializeField] private bool isPlayableFaction;
 
-    void Start()
-    {
-        if (string.IsNullOrEmpty(factionName))
-        {
-            factionName = factionType.ToString();
-        }
-    }
-
-    public void AddMember(Pirate pirate)
-    {
-        if (!members.Contains(pirate))
-        {
-            members.Add(pirate);
-             pirate.SetFaction(factionType);
-        }
-    }
-
-    public void RemoveMember(Pirate pirate)
-    {
-        if (members.Contains(pirate))
-        {
-            members.Remove(pirate);
-            pirate.SetFaction(FactionType.None); // Use None instead of null
-        }
-    }
+    public string FactionName => factionName;
+    public Sprite FactionFlag => factionFlag;
+    public Color FactionColor => factionColor;
+    public bool IsPlayableFaction => isPlayableFaction;
 
     public bool IsFriendlyWith(Faction otherFaction)
     {
-        return DiplomacySystem.Instance.AreFriendly(factionType, otherFaction.factionType);
+        if (otherFaction == null || otherFaction == this) return true;
+        float relationship = FactionManager.Instance.GetRelationship(this, otherFaction);
+        return relationship >= FactionConstants.FRIENDLY_THRESHOLD;
     }
 
     public bool IsHostileWith(Faction otherFaction)
     {
-        return DiplomacySystem.Instance.AreHostile(factionType, otherFaction.factionType);
+        if (otherFaction == null || otherFaction == this) return false;
+        float relationship = FactionManager.Instance.GetRelationship(this, otherFaction);
+        return relationship <= FactionConstants.HOSTILE_THRESHOLD;
     }
+
+    public bool IsNeutralWith(Faction otherFaction)
+    {
+        if (otherFaction == null || otherFaction == this) return false;
+        float relationship = FactionManager.Instance.GetRelationship(this, otherFaction);
+        return relationship > FactionConstants.HOSTILE_THRESHOLD && 
+               relationship < FactionConstants.FRIENDLY_THRESHOLD;
+    }
+
+    public float GetCurrentInfluence()
+    {
+        return FactionManager.Instance.GetInfluence(this);
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (string.IsNullOrEmpty(factionName))
+        {
+            factionName = name;
+        }
+    }
+#endif
 }
