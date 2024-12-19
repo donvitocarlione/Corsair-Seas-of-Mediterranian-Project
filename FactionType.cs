61ba8da2cfb6335a04b72589b147df2116f7ce3e
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-19 19:18:31 +0800

    Implement FactionType enum

---
+++ FactionType.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace CorsairGame
+{
+    public enum FactionType
+    {
+        None = 0,
+        Player = 1,
+        Pirates = 2,
+        Ottoman = 3,
+        Venice = 4,
+        Spain = 5,
+        Portugal = 6,
+        Knights = 7,
+        Barbary = 8
+    }
+}
