3639bc4a6e44988fb8052f87559ba3cca48d1f4a
donvitocarlione <105321465+donvitocarlione@users.noreply.github.com>
2024-12-30 05:16:03 +0800

    Update Faction to be a lightweight MonoBehaviour

--- Faction.cs
+++ Faction.cs
@@ -1,46 +1,22 @@
-using System.Collections.Generic;
 using UnityEngine;
 
+[AddComponentMenu("Game/Faction")]
 public class Faction : MonoBehaviour
 {
-    public FactionType factionType;
-    public string factionName;
-    public List<Pirate> members = new List<Pirate>();
-    public List<Port> controlledPorts = new List<Port>();
+    [SerializeField] private FactionType factionType;
+    public FactionType Type => factionType;
 
-    void Start()
+    private void Start()
     {
-        if (string.IsNullOrEmpty(factionName))
+        // Register with FactionManager if needed
+        if (FactionManager.Instance != null)
         {
-            factionName = factionType.ToString();
+            // Any initialization with FactionManager can be done here
         }
     }
 
-    public void AddMember(Pirate pirate)
+    private void OnDestroy()
     {
-        if (!members.Contains(pirate))
-        {
-            members.Add(pirate);
-            pirate.SetFaction(factionType);
-        }
-    }
-
-    public void RemoveMember(Pirate pirate)
-    {
-        if (members.Contains(pirate))
-        {
-            members.Remove(pirate);
-            pirate.SetFaction(FactionType.None); // Use None instead of null
-        }
-    }
-
-    public bool IsFriendlyWith(Faction otherFaction)
-    {
-        return DiplomacySystem.Instance.AreFriendly(factionType, otherFaction.factionType);
-    }
-
-    public bool IsHostileWith(Faction otherFaction)
-    {
-        return DiplomacySystem.Instance.AreHostile(factionType, otherFaction.factionType);
+        // Cleanup if needed
     }
 }
